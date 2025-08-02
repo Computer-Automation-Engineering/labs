3ba656026db3588ce32dc81843fe60a54eea030b
Jason Bragdon <jasonbragdon78@gmail.com>
2025-08-02 15:04:24 -0400

    Added First name and Last Name and checks to brint either entered

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 NameF = input("Tell me your first name ").title()
 NameL = input("Tell me your last Name: ").title()
-if NameF.strip():
+if NameF.strip() and NameL.strip():
     print(f"Hello {NameF} {NameL}!")
+elif NameF.strip():
+    print(f"Hello {NameF}!")
+elif NameL.strip():
+    print(f"Hello {NameL}!")
 else:
     print("Hello there!")
 
